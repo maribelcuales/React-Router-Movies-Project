44851d6ae2b105f20eee73324eafabd01aaad012
maribelcuales <maribelcuales@gmail.com>
2020-06-06 03:13:24 +0800

    Created Routes for MovieList and Movie components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -28,7 +30,13 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Route>
+        <MovieList />
+      </Route>
+      <Route>
+        <Movie />
+      </Route>
     </div>
   );
 };
