169b1f554793df06d55f20f1f8791c14aba973d9
maribelcuales <maribelcuales@gmail.com>
2020-06-05 19:55:33 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import SavedList from './Movies/SavedList';
+
+const App = () => {
+  const [savedList, setSavedList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovieList(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    getMovies();
+  }, []);
+
+  const addToSavedList = movie => {
+    setSavedList([...savedList, movie]);
+  };
+
+  return (
+    <div>
+      <SavedList list={savedList} />
+      <div>Replace this Div with your Routes</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/Movies/MovieList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const MovieList = props => {
+  return (
+    <div className="movie-list">
+      {props.movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+}
+
+function MovieDetails({ movie }) {
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MovieList;
