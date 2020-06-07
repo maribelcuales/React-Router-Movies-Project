c1f6538b250b33c7c68d656106a2f1e19a6c1f92
maribelcuales <maribelcuales@gmail.com>
2020-06-07 21:01:31 +0800

    Set the id const in the useEffect hook to grab id from the retrieved url.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {useParams} from "react-router-dom";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  
   useEffect(() => {
-    const id = 1;
+    const id = () => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -17,7 +18,7 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-
+    }
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
