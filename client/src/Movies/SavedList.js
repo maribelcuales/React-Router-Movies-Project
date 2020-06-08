b2bf3ed1a3ffa442ae84ccf56b2b01b5ea6e6a6b
maribelcuales <maribelcuales@gmail.com>
2020-06-09 04:52:47 +0800

    Added functionality to the Home button navigating back to Home.Also set the Home Link text-decoration to none.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.css';
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,7 +8,11 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div className="home-button">
+      <Link className="home-button-link" to="/">
+      Home
+      </Link>
+    </div>
   </div>
 );
 
