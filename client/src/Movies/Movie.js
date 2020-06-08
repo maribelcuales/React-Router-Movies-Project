aac91207272a881b039676df22ba0efbbae9eb11
maribelcuales <maribelcuales@gmail.com>
2020-06-08 23:38:29 +0800

    Set Movie to return MovieCard component.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {useParams} from "react-router-dom";
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   
   useEffect(() => {
     const id = () => {
+      // const id = useParams();
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -34,7 +36,8 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+      <MovieCard />
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -49,7 +52,7 @@ const Movie = (props) => {
             {star}
           </div>
         ))}
-      </div>
+      </div> */}
       <div className="save-button">Save</div>
     </div>
   );
