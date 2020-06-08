2f536abbcd6bc477cef49e95b24e140ce354ac20
maribelcuales <maribelcuales@gmail.com>
2020-06-08 15:36:43 +0800

    Set movie-card in MovieList to be a link directing users to the id of the individual movie.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const MovieList = props => {
   return (
-    <div className="movie-list">
+    <div className="movie-list">            
       {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
@@ -14,6 +15,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
+      <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,12 +24,12 @@ function MovieDetails({ movie }) {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
       ))}
+      </Link>
     </div>
   );
 }
