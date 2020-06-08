e3d3785a6d87e2df48b64cfc3d229c2902eef0a3
maribelcuales <maribelcuales@gmail.com>
2020-06-08 23:29:02 +0800

    Set up MovieDetails here in MovieCard.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import "./styles.css"; 
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({movie}) => {
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
