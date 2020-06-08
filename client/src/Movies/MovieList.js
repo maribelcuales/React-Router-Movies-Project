eb98f250edf80d172926e79a8bcc53501c01e101
maribelcuales <maribelcuales@gmail.com>
2020-06-08 23:30:46 +0800

    Map over MovieCard component and wrapped it in Link,

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import MovieCard from "./MovieCard";
 import "./styles.css"; 
 
 const MovieList = props => {
   return (
     <div className="movie-list">            
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link className="link-wrapper" to={`/movies/${movie.id}`}>
+        <MovieCard key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <Link className="link-wrapper" to={`/movies/${movie.id}`}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-      </Link>
-    </div>
-  );
-}
+// function MovieDetails({ movie }) {
+//   const { title, director, metascore, stars } = movie;
+//   return (
+//     <div className="movie-card">      
+//       <h2>{title}</h2>
+//       <div className="movie-director">
+//         Director: <em>{director}</em>
+//       </div>
+//       <div className="movie-metascore">
+//         Metascore: <strong>{metascore}</strong>
+//       </div>
+//       <h3>Actors</h3>
+//       {stars.map(star => (
+//         <div key={star} className="movie-star">
+//           {star}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default MovieList;
