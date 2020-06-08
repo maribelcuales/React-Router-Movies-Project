dd264505d18ede710f54c5c19562c5df21d2b718
maribelcuales <maribelcuales@gmail.com>
2020-06-09 04:36:09 +0800

    Removed unused useRouteMatch.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useParams, useRouteMatch} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
